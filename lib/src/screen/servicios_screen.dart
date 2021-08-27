8799701f90d678a314215b94f57d6b625a970dfc
PerairaCC <carloscoto0103@gmail.com>
2021-08-26 22:10:26 -0600

    Pantalla de otros servicios implementado

--- lib/src/screen/servicios_screen.dart
+++ lib/src/screen/servicios_screen.dart
@@ -9,8 +9,49 @@ class ServiciosScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBarCustom(),
       drawer: DrawerCustom(),
-      body: Center(
-        child: Text('Servicios Screen'),
+      body: SafeArea(
+        child: SingleChildScrollView(   
+          physics: BouncingScrollPhysics(),
+          child: Container(  
+            child: Column(  
+              children: [
+
+                TitleCustom(texto: 'Otros Servicios'),
+
+                ImageCustom(urlImage: 'assets/servicios/servicio1.jpg'),
+
+                SubtitleCustom(
+                  size: 25, 
+                  texto: 'Más desarrollo',
+                  center: false,
+                ),
+
+                ParrafoCustom(
+                  texto : 'Podemos ser su opción para desarrollar en la tecnología de su necesidad, tenemos experiencia en las siguientes tecnologías:',
+                ),
+
+                SizedBox(height: 10),
+
+                ParrafoCustom(
+                  texto : '■ .NET (Desktop, Web, Móvil) ' + '\n' +
+                          '■ Java (Desktop, Web, Móvil)' + '\n' +
+                          '■ Html5, CSS3, Bootstrap, JQuery' + '\n' +
+                          '■ Classic ASP' + '\n' +
+                          '■ C, C++' + '\n' +
+                          '■ Visual Basic 6' + '\n' +
+                          '■ Entre otras',
+                ),
+
+                SizedBox(height: 50),
+
+                FooterCustom(),
+
+                SizedBox(height: 30),
+
+              ],
+            ),
+          ),
+        ),
      ),
    );
   }
